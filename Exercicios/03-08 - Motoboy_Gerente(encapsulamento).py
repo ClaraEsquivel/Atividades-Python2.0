49ee36d4087fc1b6dc2b1b9449ccaff88ce6da47
ClaraEsquivel <160122503+ClaraEsquivel@users.noreply.github.com>
2024-09-09 12:42:16 +0000

    aula 09/09

--- Exercicios/03-08 - Motoboy_Gerente(encapsulamento).py
+++ Exercicios/03-08 - Motoboy_Gerente(encapsulamento).py
@@ -20,16 +20,17 @@ class Endereco:
                 )
     
 class Funcionario(ABC):
-    def __init__(self, nome:str, email:str, endereco:Endereco) -> None:
+    def __init__(self, nome:str, email:str, salario:float, endereco:Endereco) -> None:
         self.nome = nome
         self.email = email
+        self.salario = salario
         self.endereco = endereco
 
     def __str__(self) -> str:
         return(
             f"\nNome: {self.nome}"
             f"\nEmail: {self.email}"
-            # f"\nSálario: {self.salario_final}"
+            f"\nSálario: {self.salario}"
             f"\n= Endereço = {self.endereco}"
                 )    
     
@@ -41,7 +42,9 @@ class Funcionario(ABC):
         try:
             self.__verificar_salario(valor)
         except ValorNegativoError as error:
-            return f"Erro: {error}"    
+            return f"Erro: {error}"   
+
+        return self.salario
         
     def __verificar_salario(self,valor):
         if valor < 0:
@@ -50,8 +53,8 @@ class Funcionario(ABC):
 
 
 class Motoboy(Funcionario):
-    def __init__(self, nome: str, email: str, cnh:str, endereco: Endereco) -> None:
-        super().__init__(nome, email, endereco)
+    def __init__(self, nome: str, email: str, cnh:str, salario:float, endereco: Endereco) -> None:
+        super().__init__(nome, email, salario, endereco)
         self.cnh = cnh
         
 
@@ -62,17 +65,17 @@ class Motoboy(Funcionario):
         
 
 class Gerente(Funcionario):
-    def __init__(self, nome: str, email: str, endereco: Endereco) -> None:
-        super().__init__(nome, email, endereco)
+    def __init__(self, nome: str, email: str, salario:float, endereco: Endereco) -> None:
+        super().__init__(nome, email, salario, endereco)
        
 
 
-motoboy1 = Motoboy("João", "joao@gmail.com", "A", Endereco("Castelo Branco", "123", "Salvador"))
+motoboy1 = Motoboy("João", "joao@gmail.com", "A", 2000, Endereco("Castelo Branco", "123", "Salvador"))
 print(motoboy1)
-print("= Salário =")
-print(motoboy1.salario_final(-2000))
+# print("= Salário =")
+print(motoboy1.salario_final(2000))
 
-gerente1 = Gerente("Pedro", "pedro@gmail.com", Endereco("Liberdade", "115", "São Paulo"))
+gerente1 = Gerente("Pedro", "pedro@gmail.com", 5000, Endereco("Liberdade", "115", "São Paulo"))
 print(gerente1)
-print("= Salário =")
+# print("= Salário =")
 print(gerente1.salario_final(-5000))
